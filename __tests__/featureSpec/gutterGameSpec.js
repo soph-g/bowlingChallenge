15396283c3b21c592e3bbb7dfab5137619f19bed
Sophie Gill <sophie@makersacademy.com>
2019-10-31 15:49:47 +0000

    calculates a single frame score

--- __tests__/featureSpec/gutterGameSpec.js
+++ __tests__/featureSpec/gutterGameSpec.js
@@ -23,14 +23,14 @@ describe("Gutter Game", () => {
         expect(scorecard.addRoll(0)).toEqual([0, 0]);
       });
 
-      it("only allows 10 rolls", () => {
-        for (var i = 0; i < 10; i++) {
+      it("only allows 20 rolls", () => {
+        for (var i = 0; i < 20; i++) {
           scorecard.addRoll(0)
         }
-        expect(scorecard.addRoll(0)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        expect(scorecard.addRoll(0)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
       });
 
-      it("stores specified input for up to 10 rolls where no bonuses are scored", () => {
+      it("stores specified input for up to 20 rolls where no bonuses are scored", () => {
         scorecard.addRoll(0)
         scorecard.addRoll(9)
         scorecard.addRoll(1)
@@ -41,7 +41,32 @@ describe("Gutter Game", () => {
         scorecard.addRoll(6)
         scorecard.addRoll(4)
         scorecard.addRoll(5)
-        expect(scorecard.addRoll(10)).toEqual([0, 9, 1, 8, 2, 7, 3, 6, 4, 5])
+        scorecard.addRoll(0)
+        scorecard.addRoll(9)
+        scorecard.addRoll(1)
+        scorecard.addRoll(8)
+        scorecard.addRoll(2)
+        scorecard.addRoll(7)
+        scorecard.addRoll(3)
+        scorecard.addRoll(6)
+        scorecard.addRoll(4)
+        scorecard.addRoll(5)
+        expect(scorecard.addRoll(10)).toEqual([0, 9, 1, 8, 2, 7, 3, 6, 4, 5, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5])
+      });
+    });
+  });
+
+  describe("#calculateScores", () => {
+    it("returns an empty array when there are no complete frames", () => {
+      scorecard.addRoll(0);
+      expect(scorecard.calculateScores()).toEqual([]);
+    });
+
+    describe('no bonuses', () => {
+      it("returns the score when there is a single complete frame", () => {
+        scorecard.addRoll(2);
+        scorecard.addRoll(7);
+        expect(scorecard.calculateScores()).toEqual([9])
       });
     });
   });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,9 +1,21 @@
 const Scorecard = function() {
   var rolls = [];
+  // var frameScores = [];
 
   var addRoll = function(pinCount) {
-    if (validRoll(pinCount) && activeGame()) rolls.push(pinCount)
+    if (validRoll(pinCount) && activeGame()) rolls.push(pinCount);
     return rolls;
+  }
+
+  var calculateScores = function() {
+    result = []
+    if ( rolls.length > 1) {
+      var score = rolls.reduce((roll1, roll2) => {
+        return roll1 + roll2;
+      })
+      result.push(score)
+    }
+    return result;
   }
 
   var validRoll = function(roll) {
@@ -11,11 +23,12 @@ const Scorecard = function() {
   }
 
   var activeGame = function() {
-    return rolls.length < 10
+    return rolls.length < 20
   }
 
   return {
-    addRoll: addRoll
+    addRoll: addRoll,
+    calculateScores: calculateScores
   };
 }
 
